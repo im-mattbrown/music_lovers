90f7cf72c630b7c6d16411f62fdc6079c043818c
mattericbrown <matt.eric.brown@gmail.com>
2016-09-13 22:43:28 -0700

    remove validations add link to signup from login page, edit the seeking page rick rolled the video page for checking heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,28 +2,9 @@ class User < ApplicationRecord
   has_secure_password
 
   cattr_accessor :current_user
+  #attr_accessible :link
 
   SEEKING = ['f4f', 'f4m', 'm4f', 'm4m']
-
-  # validates :first_name, :last_name, :password, :seeking, :age, :answer1, :answer2, :answer3, :answer4, :answer5, :answer6, :answer7, :playlist, :suitors, :location, :profile_photo,
-  #   presence: true
-  #
-  # validates :username,
-  #   presence: true,
-  #   uniqueness: true,
-  #   length: { minimum: 1, maximum: 50 }
-  #
-  # validates :profile_video,
-  #   presence: true,
-  #   length: {minimum: 1, maximum: 11 }
-
-  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
-  # validates :email,
-  #   presence: true,
-  #   format: { with: EMAIL_REGEX},
-  #   uniqueness: true,
-  #   length: { maximum: 50 }
 
     def self.confirm(params)
       @user = User.where("email ILIKE ?", params[:email]).first
