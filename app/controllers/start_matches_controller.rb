95bd6fc2d8dbf068f0398ff14032ed01e236dc21
mattericbrown <matt.eric.brown@gmail.com>
2016-09-14 20:37:21 -0700

    mobile styling, add footer to browser heroku commit 833pm

--- app/controllers/start_matches_controller.rb
+++ app/controllers/start_matches_controller.rb
@@ -42,6 +42,9 @@ before_action :find_user, only: [:video, :answers, :playlist, :profile]
     next_id = @@potential_matches.shift
     # this will be the user
     @@next_match = User.find(next_id)
+    if @@potential_matches.length == 0
+      flash[:notice] = "You have ran out of matches. Try again later or lower your standards."
+    end
     redirect_to user_video_path and return
   end
 
